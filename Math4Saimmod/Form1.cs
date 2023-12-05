35b5a8a2ca60fd54753b24626b0c55a342f9379c
mc3Kast <k3sha2014@yandex.ru>
2023-12-05 04:47:10 +0300

    Counting total arriving and processing times

--- Math4Saimmod/Form1.cs
+++ Math4Saimmod/Form1.cs
@@ -17,15 +17,20 @@ namespace Math4Saimmod
             double p3 = p1 / p2;
             double v = (double)1 / 3 + 1;
             Model model = new Model((float)p1, (float)p2);
-            model.Run(n);
+            ModelIntensity modelI = new ModelIntensity(p1, p2);
+            modelI.Run(n);
             //double loch = (p3 * p3 * v) / (2 * ( 1- p3));
             //double lc = (p3 * p3 * v) / (2 * (1 - p3)) + p3;
             //double woch = (p3 * p3 * v) / (2 * p1 * (1 - p3));
             //double wc = (p3 * p3 * v) / (2 * p1 * (1 - p3)) + 1 / p2;
-            float loch = (float)model.processor1.queueTicks / n;
+            /*float loch = (float)model.processor1.queueTicks / n;
             float lc = (float)(model.processor1.queueTicks + model.processor1._tickCount) / (float)n;
             float woch = model.processor1._timeInQueue / n;
-            float wc = (model.processor1._time + model.processor1._timeInQueue) / n;
+            float wc = (model.processor1._time + model.processor1._timeInQueue) / n;*/
+            double loch = modelI.averageRequestsInQueue;
+            double lc = modelI.averageRequestsInSystem;
+            double woch = modelI.averageTimeInQueue;
+            double wc = modelI.averageTimeInSystem;
 
             lbLoch.Text = $"Loch: {loch}";
             lbLc.Text = $"Lc: {lc}";
--- Math4Saimmod/Lib/Elements/ModelIntensity.cs
+++ Math4Saimmod/Lib/Elements/ModelIntensity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Math4Saimmod.Lib.LemRandom;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,103 @@ namespace Math4Saimmod.Lib.Elements
 {
     internal class ModelIntensity
     {
+        private Queue<double> arrivalTimes = new Queue<double>();
+        private double serviceRate;
+        private double lambda;
+        public double averageRequestsInSystem;
+        public double averageRequestsInQueue;
+        public double averageTimeInSystem;
+        public double averageTimeInQueue;
+
+        public ModelIntensity(double lambda, double mu)
+        {
+            this.lambda = lambda;
+            this.serviceRate = mu;
+        }
+
+        public void Run(int numberOfRequests)
+        {
+            double[] Arrivals = new double[numberOfRequests];
+            double[] Processing = new double[numberOfRequests];
+            Random rand = new Random();
+            double currentTime = 0;
+            double totalWaitingTime = 0;
+            double totalSystemTime = 0;
+
+            for (int i = 0; i < numberOfRequests; i++)
+            {
+                double interArrivalTime = Distribution.ExponentialDistribution(rand.NextSingle(), lambda);
+                currentTime += interArrivalTime;
+                Arrivals[i] = currentTime;
+            }
+            double serviceTime = GenerateErlang3ServiceTime();
+            Processing[0] = Arrivals[0] + serviceTime;
+            for (int i = 1; i < numberOfRequests - 1; i++)
+            {
+                serviceTime = GenerateErlang3ServiceTime();
+                if (serviceTime + Arrivals[i - 1] < Arrivals[i])
+                {
+                    Processing[i] = Arrivals[i] + serviceTime;
+                }
+                else
+                {
+                    Processing[i] = Processing[i - 1] + serviceTime;
+                }
+            }
+
+
+
+            averageRequestsInSystem = totalSystemTime / currentTime;
+            averageRequestsInQueue = totalWaitingTime / currentTime;
+            averageTimeInSystem = totalSystemTime / numberOfRequests;
+            averageTimeInQueue = totalWaitingTime / numberOfRequests;
+        }
+
+        public void Run2(int numberOfRequests)
+        {
+            Random rand = new Random();
+            double currentTime = 0;
+            double totalWaitingTime = 0;
+            double totalSystemTime = 0;
+            int requestsInQueue = 0;
+
+            for (int i = 0; i < numberOfRequests; i++)
+            {
+                double interArrivalTime = -Math.Log(rand.NextDouble()) / lambda;
+                currentTime += interArrivalTime;
+
+                double serviceTime = GenerateErlang3ServiceTime();
+                double serviceCompletionTime = currentTime + serviceTime;
+
+                if (currentTime < serviceCompletionTime)
+                {
+                    arrivalTimes.Enqueue(currentTime);
+                }
+                else
+                {
+                    requestsInQueue++;
+                    totalWaitingTime += currentTime - arrivalTimes.Dequeue();
+                }
+
+                totalSystemTime += serviceTime + totalWaitingTime;
+            }
+
+            averageRequestsInSystem = totalSystemTime / currentTime;
+            averageRequestsInQueue = totalWaitingTime / currentTime;
+            averageTimeInSystem = totalSystemTime / numberOfRequests;
+            averageTimeInQueue = totalWaitingTime / numberOfRequests;
+        }
+
+        private double GenerateErlang3ServiceTime()
+        {
+            Random rand = new Random();
+
+            double x1 = Distribution.ExponentialDistribution(rand.NextSingle(), serviceRate);
+            double x2 = Distribution.ExponentialDistribution(rand.NextSingle(), serviceRate);
+            double x3 = Distribution.ExponentialDistribution(rand.NextSingle(), serviceRate);
+
+            return x1 + x2 + x3;
+        }
+
     }
 }
